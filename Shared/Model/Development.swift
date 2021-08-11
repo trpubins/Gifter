7e334be88c2a4b94361fa2d82d19e92c2c980bf0
Tanner Pubins <t.pubins@icloud.com>
2021-08-10 20:19:51 -0700

    Created swift file to support unit testing in development.

---
+++ Shared/Model/Development.swift
@@ -0,0 +1,28 @@
+//
+//  Development.swift
+//  Gifter
+//
+//  Created by Tanner on 8/10/21.
+//
+
+import Foundation
+
+/**
+ Logs an Appear command to the console.
+ 
+ - Parameters:
+    - title: The title of the View that is appearing
+ */
+func logAppear(title: String) {
+    print(title + " Appears")
+}
+
+/**
+ Logs a Disappear command to the console.
+ 
+ - Parameters:
+    - title: The title of the View that is disappearing
+ */
+func logDisappear(title: String) {
+    print(title + " Disappears")
+}
