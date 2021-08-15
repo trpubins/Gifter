248a67d80805a378f05e584d819ed4379cf0f16c
Tanner Pubins <t.pubins@icloud.com>
2021-08-14 17:02:29 -0700

    Further extended the Date struct to get the day of the week for the Date instance and the number of days until the Date instance arrives.

--- Shared/Helpers/Date+Extensions.swift
+++ Shared/Helpers/Date+Extensions.swift
@@ -31,6 +31,13 @@ extension Date {
         return Calendar.current.date(byAdding: .day, value: 1, to: noon)!
     }
     
+    /// The day of the week for the Date itself
+    var dayOfWeek: String {
+        let index = Calendar.current.component(.weekday, from: self)
+        let dayOfWeekAbbrev = Calendar.current.weekdaySymbols[index - 1].prefix(3)  // the abbreviated form, first 3 characters representing the day of the week
+        return String(dayOfWeekAbbrev)
+    }
+    
     /// Noontime of the Date itself
     var noon: Date {
         return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
@@ -46,9 +53,36 @@ extension Date {
         return Calendar.current.component(.year,  from: self)
     }
     
+    /// The number of days until the Date itself arrives.
+    var daysUntil: Int {
+        return Calendar(identifier: .gregorian).numberOfDaysBetween(Date(), and: self)
+    }
+    
     /// `true` if the Date is the last day of the month, `false` otherwise
     var isLastDayOfMonth: Bool {
         return dayAfter.month != month
     }
     
 }
+
+extension Calendar {
+    
+    /**
+     Returns the number of days between two dates.
+     
+     - Parameters:
+        - from: The starting Date
+        - to: The ending Date
+     
+     - Returns: The number of days between the provided Dates.
+     */
+    func numberOfDaysBetween(_ from: Date, and to: Date) -> Int {
+        let fromDate = startOfDay(for: from)
+        let toDate = startOfDay(for: to)
+        let numberOfDays = dateComponents([.day], from: fromDate, to: toDate)
+        
+        return numberOfDays.day!
+    }
+    
+}
+
