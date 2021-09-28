b91edf44a0d83e77c65b159b2cd385224b4e1e95
Tanner Pubins <t.pubins@icloud.com>
2021-09-27 23:46:53 -0700

    Validation publishers drop the first element on condition instead of every time.
    Publisher+Validation.swift
     * Added dropFirst param to most functions
     * Added documentation for each function
    
    ValidationPublishers.swift
     * Added dropFirst param to most functions
     * Refactored code by creating a reusable generic function, getValidationPublisher()
     * Added documentation for each function

--- Shared/Helpers/Publisher+Validation.swift
+++ Shared/Helpers/Publisher+Validation.swift
@@ -15,24 +15,96 @@ import Regex
 
 extension Published.Publisher where Value == String {
     
-    func nonEmptyValidator(_ errorMessage: @autoclosure @escaping ValidationErrorClosure) -> ValidationPublisher {
-        return ValidationPublishers.nonEmptyValidation(for: self, errorMessage: errorMessage())
+    /**
+     Validates whether a string property is non-empty.
+     
+     - Parameters:
+        - errorMessage: The message to provide in an invalid scenario
+        - dropFirst: `true` drops the first element from the publisher, `false` does not drop any elements
+     
+     - Returns: The validation publisher that validates against non-empty strings.
+     */
+    func nonEmptyValidator(_ errorMessage: @autoclosure @escaping ValidationErrorClosure,
+                           dropFirst: Bool) -> ValidationPublisher {
+        return ValidationPublishers.nonEmptyValidation(for: self,
+                                                       errorMessage: errorMessage(),
+                                                       dropFirst: dropFirst)
     }
     
-    func matcherValidation(_ pattern: String, _ errorMessage: @autoclosure @escaping ValidationErrorClosure) -> ValidationPublisher {
-        return ValidationPublishers.matcherValidation(for: self, withPattern: pattern.r!, errorMessage: errorMessage())
+    /**
+     Validates whether a string matches a regular expression.
+     
+     - Parameters:
+        - pattern: The regular expression pattern to match
+        - errorMessage: The message to provide in an invalid scenario
+        - dropFirst: `true` drops the first element from the publisher, `false` does not drop any elements
+     
+     - Returns: The validation publisher that validates against a regular expression.
+     */
+    func matcherValidation(_ pattern: String,
+                           _ errorMessage: @autoclosure @escaping ValidationErrorClosure,
+                           dropFirst: Bool) -> ValidationPublisher {
+        return ValidationPublishers.matcherValidation(for: self,
+                                                      withPattern: pattern.r!,
+                                                      errorMessage: errorMessage(),
+                                                      dropFirst: dropFirst)
     }
     
+    /**
+     Retrieves a publisher that will always publish valid.
+
+     - Returns: The validation publisher that always emits .success.
+     */
     func alwaysValid() -> ValidationPublisher {
         return ValidationPublishers.alwaysValid(for: self)
     }
     
 }
 
+extension Published.Publisher where Value == [WishList] {
+    
+    /**
+     Validates whether an array element in a wish list matches the provided regular expression.
+     
+     - Parameters:
+        - id: The id of the wish list used in filtering the wish list array
+        - pattern: The regular expression pattern to match
+        - errorMessage: The message to provide in an invalid scenario
+        - dropFirst: `true` drops the first element from the publisher, `false` does not drop any elements
+     
+     - Returns: The validation publisher that validates against the regular expression.
+     */
+    func wishListValidation(_ id: UUID,
+                            _ pattern: String,
+                            _ errorMessage: @autoclosure @escaping ValidationErrorClosure,
+                            dropFirst: Bool) -> ValidationPublisher {
+        return ValidationPublishers.wishListValidation(for: self,
+                                                       withId: id,
+                                                       withPattern: pattern.r!,
+                                                       errorMessage: errorMessage(),
+                                                       dropFirst: dropFirst)
+    }
+    
+}
+
 extension Published.Publisher where Value == Date {
-     func dateValidation(afterDate after: Date = .distantPast,
+     
+    /**
+     Validates whether a date falls between a date range. If one of the bounds isn't provided, a suitable distant detail is used.
+     
+     - Parameters:
+        - after: Optionally provide this date  - serves as the date validation lower bound
+        - before: Optionally provide this date  - serves as the date validation upper bound
+        - errorMessage: The message to provide in an invalid scenario
+     
+     - Returns: The validation publisher that validates against a date range.
+     */
+    func dateValidation(afterDate after: Date = .distantPast,
                          beforeDate before: Date = .distantFuture,
                          errorMessage: @autoclosure @escaping ValidationErrorClosure) -> ValidationPublisher {
-        return ValidationPublishers.dateValidation(for: self, afterDate: after, beforeDate: before, errorMessage: errorMessage())
+        return ValidationPublishers.dateValidation(for: self,
+                                                   afterDate: after,
+                                                   beforeDate: before,
+                                                   errorMessage: errorMessage())
     }
 }
--- Shared/Helpers/ValidationPublishers.swift
+++ Shared/Helpers/ValidationPublishers.swift
@@ -17,53 +17,161 @@ typealias ValidationErrorClosure = () -> String
 
 typealias ValidationPublisher = AnyPublisher<Validation, Never>
 
+/// Stores several functions for providing publishers that perform validation on user input.
 class ValidationPublishers {
 
-    /// Validates whether a string property is non-empty.
+    /**
+     Validates whether a string property is non-empty.
+     
+     - Parameters:
+        - publisher: The class property that is publishing values
+        - errorMessage: The message to provide in an invalid scenario
+        - dropFirst: `true` drops the first element from the publisher, `false` does not drop any elements
+     
+     - Returns: The validation publisher that validates against non-empty strings.
+     */
     static func nonEmptyValidation(for publisher: Published<String>.Publisher,
-                                   errorMessage: @autoclosure @escaping ValidationErrorClosure) -> ValidationPublisher {
-        return publisher.map { value in
-            guard value.count > 0 else {
-                return .failure(message: errorMessage())
+                                   errorMessage: @autoclosure @escaping ValidationErrorClosure,
+                                   dropFirst: Bool) -> ValidationPublisher {
+        
+        func getPublisherMap() -> Publishers.Map<Published<String>.Publisher, Validation> {
+            return publisher.map { value in
+                guard value.count > 0 else {
+                    return .failure(message: errorMessage())
+                }
+                return .success
             }
-            return .success
         }
-        .dropFirst()
-        .eraseToAnyPublisher()
+        
+        return getValidationPublisher(getPublisherMap(), dropFirst: dropFirst)
     }
     
-    /// Validates whether a string matches a regular expression.
+    /**
+     Validates whether a string matches a regular expression.
+     
+     - Parameters:
+        - publisher: The class property that is publishing values
+        - pattern: The regular expression pattern to match
+        - errorMessage: The message to provide in an invalid scenario
+        - dropFirst: `true` drops the first element from the publisher, `false` does not drop any elements
+     
+     - Returns: The validation publisher that validates against a regular expression.
+     */
     static func matcherValidation(for publisher: Published<String>.Publisher,
                                   withPattern pattern: Regex,
-                                  errorMessage: @autoclosure @escaping ValidationErrorClosure) -> ValidationPublisher {
-        return publisher.map { value in
-            guard pattern.matches(value) else {
-                return .failure(message: errorMessage())
+                                  errorMessage: @autoclosure @escaping ValidationErrorClosure,
+                                  dropFirst: Bool) -> ValidationPublisher {
+        
+        func getPublisherMap() -> Publishers.Map<Published<String>.Publisher, Validation> {
+            return publisher.map { value in
+                guard pattern.matches(value) else {
+                    return .failure(message: errorMessage())
+                }
+                return .success
             }
-            return .success
         }
-        .dropFirst()
-        .eraseToAnyPublisher()
+        
+        return getValidationPublisher(getPublisherMap(), dropFirst: dropFirst)
     }
     
-    /// Always returns .success.
+    /**
+     Retrieves a publisher that will always publish valid.
+     
+     - Parameters:
+        - publisher: The class property that is publishing values
+     
+     - Returns: The validation publisher that always emits .success.
+     */
     static func alwaysValid(for publisher: Published<String>.Publisher) -> ValidationPublisher {
-        return publisher.map { value in
-            return .success
+        
+        func getPublisherMap() -> Publishers.Map<Published<String>.Publisher, Validation> {
+            return publisher.map { value in
+                return .success
+            }
         }
-        .dropFirst()
-        .eraseToAnyPublisher()
+
+        return getValidationPublisher(getPublisherMap(), dropFirst: false)
     }
     
-    /// Validates whether a date falls between two other dates. If one of the bounds isn't provided, a suitable distant detail is used.
+    /**
+     Validates whether an array element in a wish list matches the provided regular expression.
+     
+     - Parameters:
+        - publisher: The class property that is publishing values
+        - id: The id of the wish list used in filtering the wish list array
+        - pattern: The regular expression pattern to match
+        - errorMessage: The message to provide in an invalid scenario
+        - dropFirst: `true` drops the first element from the publisher, `false` does not drop any elements
+     
+     - Returns: The validation publisher that validates against the regular expression.
+     */
+    static func wishListValidation(for publisher: Published<Array<WishList>>.Publisher,
+                                   withId id: UUID,
+                                   withPattern pattern: Regex,
+                                   errorMessage: @autoclosure @escaping ValidationErrorClosure,
+                                   dropFirst: Bool) -> ValidationPublisher {
+        
+        func getPublisherMap() -> Publishers.Map<Published<Array<WishList>>.Publisher, Validation> {
+            return publisher.map { array in
+                if let value = array.first(where: { $0.id == id }) {
+                    guard pattern.matches(value.url) else {
+                        return .failure(message: errorMessage())
+                    }
+                    return .success
+                } else {
+                    return .success
+                }
+            }
+        }
+        
+        return getValidationPublisher(getPublisherMap(), dropFirst: dropFirst)
+    }
+    
+    /**
+     Validates whether a date falls between a date range. If one of the bounds isn't provided, a suitable distant detail is used.
+     
+     - Parameters:
+        - publisher: The class property that is publishing values
+        - after: Optionally provide this date  - serves as the date validation lower bound
+        - before: Optionally provide this date  - serves as the date validation upper bound
+        - errorMessage: The message to provide in an invalid scenario
+     
+     - Returns: The validation publisher that validates against a date range.
+     */
     static func dateValidation(for publisher: Published<Date>.Publisher,
                                afterDate after: Date = .distantPast,
                                beforeDate before: Date = .distantFuture,
                                errorMessage: @autoclosure @escaping ValidationErrorClosure) -> ValidationPublisher {
-        return publisher.map { date in
-            return date > after && date < before ? .success : .failure(message: errorMessage())
+        
+        func getPublisherMap() -> Publishers.Map<Published<Date>.Publisher, Validation> {
+            return publisher.map { date in
+                return date > after && date < before ? .success : .failure(message: errorMessage())
+            }
         }
-        .eraseToAnyPublisher()
+        
+        return getValidationPublisher(getPublisherMap(), dropFirst: false)
+    }
+    
+    /**
+     Retrieves the validation publisher and determines if the first element published should be dropped.
+     
+     - Parameters:
+        - publisherMap: Maps a publisher to a validation state
+        - dropFirst: `true` drops the first element from the publisher, `false` does not drop any elements
+    
+     - Returns: The validation publisher with the first element dropped or not.
+     */
+    private static func getValidationPublisher<T>(_ publisherMap: Publishers.Map<Published<T>.Publisher, Validation>,
+                                                  dropFirst: Bool) -> ValidationPublisher {
+        // drop the first element published on condition
+        if dropFirst {
+            return publisherMap
+                .dropFirst()
+                .eraseToAnyPublisher()
+        } else {
+            return publisherMap
+                .eraseToAnyPublisher()
+        }
     }
 
 }
